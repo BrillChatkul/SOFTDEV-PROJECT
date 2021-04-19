b818e0610d59612a6721a5e14f772b586f3ebc4c
Chatkul Rattanarithikul <billchartkul@hotmail.com>
2021-04-19 21:31:49 +0700

    เพิ่มเงื่อนไข

--- softdev-petme/src/components/contract.js
+++ softdev-petme/src/components/contract.js
@@ -6,7 +6,7 @@ import GoogleMapReact from 'google-map-react';
 
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import { faAlignJustify, fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 
@@ -35,9 +35,69 @@ const Home2 = ()=> {
         classStyle == 'menu-header'?setClassStyle('menu-header active'):setClassStyle('menu-header')
         classStyle == 'menu-header active'?setAstStyle('ast-fullbg'):setAstStyle('ast')
         console.log({astStyle})
-
-    
     }
+    // ----------------------------------------------------------------condition contract--------------------------------------------
+    const [Name,setName] = useState(null)                       /////////////////Name-Surname
+    const [alertBoxName,setAlertBoxName] = useState(false)  
+    const [NameBorder,setNameBorder] = useState('inputTextB')
+    function CheckName(val){
+        if(val.target.value.length <= 0)
+        {setAlertBoxName(true); setNameBorder('inputTextB-alert')}
+        else{setAlertBoxName(false); setNameBorder('inputTextB')}
+    }
+    function getName(val){setName(val.target.value)}
+
+    const [Email,setEmail] = useState(null)                     /////////////////Email
+    const [alertBoxEmail,setAlertBoxEmail] = useState(false)   
+    const [EmailBorder,setEmailBorder] = useState('inputTextB')
+    function CheckEmail(val){
+        if(val.target.value.length <= 0 || !val.target.value.includes('@') || !(val.target.value.includes('com')||val.target.value.includes('co.th')))
+        {setAlertBoxEmail(true); setEmailBorder('inputTextB-alert')}
+        else{setAlertBoxEmail(false); setEmailBorder('inputTextB')}
+    }
+    function getEmail(val){setEmail(val.target.value)}
+
+    const [alertBoxPhone,setAlertBoxPhone] = useState(false)  /////////////////PhoneNumber
+    const [PhoneBorder,setPhoneBorder] = useState('inputTextB')
+    const [PhoneNumber,setPhoneNumber] = useState(null)
+    function CheckPhone(val){
+        if(val.target.value.length != 10)
+        {setAlertBoxPhone(true); setPhoneBorder('inputTextB-alert')}
+        else{setAlertBoxPhone(false); setPhoneBorder('inputTextB')}
+    }
+    function NumberOnly(val){
+        const re = /^[0-9\b]+$/;
+          if (val.target.value === '' || re.test(val.target.value)) {
+            setPhoneNumber(val.target.value)
+          }
+    }
+
+    const [Topic,setTopic] = useState(null)                 /////////////////Topic
+    const [alertBoxTopic,setAlertBoxTopic] = useState(false)  
+    const [TopicBorder,setTopicBorder] = useState('inputTextB')
+    function CheckTopic(val){
+        if(val.target.value.length <= 0)
+        {setAlertBoxTopic(true); setTopicBorder('inputTextB-alert')}
+        else{setAlertBoxTopic(false); setTopicBorder('inputTextB')}
+    }
+    function getTopic(val){setTopic(val.target.value)}
+
+    const [Textbox,setTextbox] = useState(null)                /////////////////Textbox 
+    const [alertBoxTextbox ,setAlertBoxTextbox ] = useState(false)  
+    const [TextboxBorder,setTextboxBorder] = useState('inputTextBLong')
+    function CheckTextbox (val){
+        if(val.target.value.length <= 0)
+        {setAlertBoxTextbox (true); setTextboxBorder('inputTextBLong-alert')}
+        else{setAlertBoxTextbox(false); setTextboxBorder('inputTextBLong')}
+    }
+    function getTextbox(val){setTextbox(val.target.value)}
+
+    function CheckAll(){
+        CheckName(Name);
+    }
+    // ----------------------------------------------------------------condition contract End--------------------------------------------
+    
+    
 
     return (
         <div>
@@ -75,11 +135,7 @@ const Home2 = ()=> {
                     <h1 className = 'headText'>ที่อยู่</h1>
                     <div className = 'textBox'>
                         <img className='logoAddress' src={Bell_icon_src}></img>
-                        <div>
-                            <p className = 'textAddress'>เลขที่ 1 ซอยฉลองกรุง 1 เขตลาดกระบัง กรุงเทพมหานคร ประเทศไทย 10520</p>
-                            
-                        </div>
-                        
+                        <p className = 'textAddress'>เลขที่ 1 ซอยฉลองกรุง 1 เขตลาดกระบัง กรุงเทพมหานคร ประเทศไทย 10520</p>
                     </div>
                     <div className = 'textBox'>
                         <img className='logoAddress' src={Bell_icon_src}></img>
@@ -95,15 +151,33 @@ const Home2 = ()=> {
                 <div className = 'contractBox'>
                         <h1 className = 'headText'>ติดต่อเรา</h1>
                         <div className = 'inputLayer'>
-                            <input id="name" type="text" class ='inputTextB'placeholder="ชื่อจริง-นามสกุล" maxlength="40"></input>
-                            <input type="text" class ='inputTextB'placeholder="อีเมล"></input>
+                            <div>
+                                <input name="Name" type="text" class ={NameBorder} placeholder="ชื่อจริง-นามสกุล" onInput={getName} maxlength="40" onBlur={CheckName} value={Name}></input>
+                                {alertBoxName?<p className='alert-text'>กรุณากรอกชื่อ</p>:null}
+                            </div>
+                            
+                            <div>
+                                <input type="text" class ={EmailBorder}placeholder="อีเมล" onInput={getEmail} onBlur={CheckEmail} maxlength="50" value={Email}></input>
+                                {alertBoxEmail?<p className='alert-text'>กรุณากรอกอีเมลให้ถูกต้อง</p>:null}
+                            </div>
+                            
                         </div>
                         <div className = 'inputLayer'>
-                            <input type="text" class ='inputTextB'placeholder="เบอร์โทรศัพท์" maxlength="10" pattern="[0-9]*"></input>
-                            <input type="text" class ='inputTextB'placeholder="หัวข้อ"></input>
+                            <div>
+                                <input type="text" class ={PhoneBorder}placeholder="เบอร์โทรศัพท์" onInput={NumberOnly} onBlur={CheckPhone} maxlength="10" value={PhoneNumber}></input>
+                                {alertBoxPhone?<p className='alert-text'>กรุณากรอกเบอร์ให้ถูกต้อง 0xxxxxxxxx</p>:null}
+                            </div>
+                            
+                            <div>
+                                <input type="text" class ={TopicBorder}placeholder="หัวข้อ" onInput={getTopic} onBlur={CheckTopic} maxlength="60" value={Topic}></input>
+                                {alertBoxTopic?<p className='alert-text'>กรุณากรอกหัวข้อ</p>:null}
+                            </div>
                         </div>
-                        <textarea type="text" class ='inputTextBLong'placeholder="ข้อความ" maxlength="100"></textarea>
-                        <button className='buttonSumit'>ส่ง</button>
+                        <textarea type="text" class = {TextboxBorder} placeholder="ข้อความ" onInput={getTextbox} onBlur={CheckTextbox} maxlength="1000" value={Textbox}></textarea>
+                        {alertBoxTextbox?<p className='alert-text'>กรุณาพิมพ์เนื้อหา</p>:null}
+                        
+                        
+                        <button className='buttonSumit' onClick={CheckAll}>ส่ง</button>
                 </div>
                 </div>
                 </div>
